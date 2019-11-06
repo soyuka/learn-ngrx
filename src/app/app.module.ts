bcc0f7f31a65c9ef6ab0f5a8b16712c1cc495f14
soyuka <soyuka@users.noreply.github.com>
2019-11-06 16:06:42 +0100

    counter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,18 @@ import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { appReducer } from './app.reducer';
+import { counterReducer } from './counter/counter.duck';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({app: appReducer}),
+    StoreModule.forRoot({app: appReducer, count: counterReducer}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production  })
   ],
   providers: [],
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { increment, decrement, reset } from './counter.duck';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.scss']
+})
+export class CounterComponent {
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.pipe(select('count'));
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
+}
